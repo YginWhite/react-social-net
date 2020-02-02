f845a07d879ef03aa7661d65f15d5a1bf22aaa26
Yrii Sklyarov <yginwhite@gmail.com>
2020-02-02 19:14:28 +0200

    Move api functionality from Users component to its container

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,84 +1,62 @@
 import React from 'react';
 import clss from './Users.module.css';
-import * as axios from 'axios';
 
-class Users extends React.Component {
-	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-			 .then(response => {
-			 	this.props.setUsers(response.data.items);
-			 	this.props.setUsersCount(response.data.totalCount);
-			 });
-	}
+const defaultImg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS59F9C4DK6066H7NHNgZZXg_gxBbCEfE4ta9enVNq1953lDO4Qg&s`;
 
-	changePage(pageNumber) {
-		this.props.setPage(pageNumber);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-			 .then(response => {
-			 	this.props.setUsers(response.data.items);
-			 });
-	}
 
-	changeButtonsRange(step, direction, pagesAmount) {
-		this.props.changePagesRange(step, direction, pagesAmount);
-	}
+const Users = (props) => {
+	const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	const pages = Array(pagesCount).fill(1);
 
-	render() {
-		const defaultImg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS59F9C4DK6066H7NHNgZZXg_gxBbCEfE4ta9enVNq1953lDO4Qg&s`;
-		
-		const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-		const pages = Array(pagesCount).fill(1);
+	const buttonsRange = props.currentPagesRange;
+	const buttonsCount = props.currentPagesCount;
+	const currentPage = props.currentPage;
 
-		const buttonsRange = this.props.currentPagesRange;
-		const buttonsCount = this.props.currentPagesCount;
-		const currentPage = this.props.currentPage;
-
-		return (
-			<div className={clss.users}>
-				<div className={clss.users_pagination}>
-					<button onClick={(e) => {this.changeButtonsRange(buttonsCount, '<', pagesCount)}}>{'<'}</button>
-					{pages.map((p, i) => {
-						const n = i + 1;
-						let cls = this.props.currentPage === n ? clss.users_pagination_btn__curr : '';
-						if (n < buttonsRange[0] || n > buttonsRange[1]) return null;
-						return (
-							<span key={n} 
-								  className={`${clss.users_pagination_btn} ${cls}`}
-								  onClick={(e) => {this.changePage(n)}}
-							>
-								{n}
-							</span>
-						);
-					})}
-					<button onClick={(e) => {this.changeButtonsRange(buttonsCount, '>', pagesCount)}}>{'>'}</button>
-				</div>
-
-				{this.props.users.map((user) => {
+	return (
+		<div className={clss.users}>
+			<div className={clss.users_pagination}>
+				<button onClick={(e) => {props.changePagesRange(buttonsCount, '<', pagesCount)}}>{'<'}</button>
+				{pages.map((p, i) => {
+					const n = i + 1;
+					let cls = props.currentPage === n ? clss.users_pagination_btn__curr : '';
+					if (n < buttonsRange[0] || n > buttonsRange[1]) return null;
 					return (
-						<div key={user.id} className={clss.users_user}>
-							<figure className={clss.users_user_pict}>
-								<p>
-									{user.photos.small ? <img src={user.photos.small}/> :
-														 <img src={defaultImg}/>}
-								</p>	
-								<figcaption>{user.fullName ? user.fullName : user.name}</figcaption>
-							</figure>
-							<ul className={clss.users_user_list}>
-								<li><span>Country: </span>{"user.location.country"}</li>
-								<li><span>City: </span>{"user.location.city"}</li>
-								<li><span>Status: </span>{user.status}</li>
-							</ul>
-							<div className={clss.users_user_btn}>
-								{user.followed ? 
-									<button onClick={() => {this.props.unfollow(user.id)}}>unfollow</button> : 
-									<button onClick={() => {this.props.follow(user.id)}}>follow</button>}
-							</div>
-						</div>
+						<span key={n} 
+							  className={`${clss.users_pagination_btn} ${cls}`}
+							  onClick={(e) => {props.changePage(e, n)}}
+						>
+							{n}
+						</span>
 					);
 				})}
+				<button onClick={(e) => {props.changePagesRange(buttonsCount, '>', pagesCount)}}>{'>'}</button>
 			</div>
-		);
-	}
+
+			{props.users.map((user) => {
+				return (
+					<div key={user.id} className={clss.users_user}>
+						<figure className={clss.users_user_pict}>
+							<p>
+								{user.photos.small ? <img src={user.photos.small}/> :
+													 <img src={defaultImg}/>}
+							</p>	
+							<figcaption>{user.fullName ? user.fullName : user.name}</figcaption>
+						</figure>
+						<ul className={clss.users_user_list}>
+							<li><span>Country: </span>{"user.location.country"}</li>
+							<li><span>City: </span>{"user.location.city"}</li>
+							<li><span>Status: </span>{user.status}</li>
+						</ul>
+						<div className={clss.users_user_btn}>
+							{user.followed ? 
+								<button onClick={() => {props.unfollow(user.id)}}>unfollow</button> : 
+								<button onClick={() => {props.follow(user.id)}}>follow</button>}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,6 +4,42 @@ import Users from './Users';
 import { followActionCreator, unfollowActionCreator,
 		 setUsersActionCreator, setCurrentPageActionCreator,
 		 setTotalUsersCountActionCreator, changePagesRangeActionCreator } from './../../redux/usersReducer';
+import * as axios from 'axios';
+
+
+class UsersContainer extends React.Component {
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+			 .then(response => {
+			 	this.props.setUsers(response.data.items);
+			 	this.props.setUsersCount(response.data.totalCount);
+			 });
+	}
+
+	changePage = (e, pageNumber) => {
+		this.props.setPage(pageNumber);
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+			 .then(response => {
+			 	this.props.setUsers(response.data.items);
+			 });
+	}
+
+	render() {
+		return <Users users={this.props.users}
+					  pageSize={this.props.pageSize}
+					  totalUsersCount={this.props.totalUsersCount}
+					  
+					  currentPage={this.props.currentPage}
+					  currentPagesCount={this.props.currentPagesCount}
+					  currentPagesRange={this.props.currentPagesRange}
+					  
+					  follow={this.props.follow}
+					  unfollow={this.props.unfollow}
+					  changePagesRange={this.props.changePagesRange}
+
+					  changePage={this.changePage}/>
+	}
+}
 
 const mapStateToProps = (state) => {
 	return { 
@@ -44,6 +80,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
