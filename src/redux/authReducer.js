82dfc1582b5421620e16b1ac677e39210cfc6aff
Yrii Sklyarov <yginwhite@gmail.com>
2020-02-04 13:22:51 +0200

    Create authReducer

---
+++ src/redux/authReducer.js
@@ -0,0 +1,20 @@
+const SET_USER_DATA = 'SET-USER-DATA';
+
+const initialState = {
+	userId: null,
+	email: null,
+	login: null
+};
+
+const authReducer = (state = initialState, action) => {
+	switch(action.type) {
+		case SET_USER_DATA:
+			return { ...state, ...action.data };
+		default:
+			return state;
+	}
+};
+
+export default authReducer;
+
+export const setUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}});
