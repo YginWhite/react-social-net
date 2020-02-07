f1875d7258195ccf7ae48b34627fb18a79e53259
Yrii Sklyarov <yginwhite@gmail.com>
2020-02-07 14:25:30 +0200

    Create Login component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
+import Login from './components/Login/Login';
 
 const App = (props) => {
 	return (
@@ -29,6 +30,8 @@ const App = (props) => {
 					<Route path='/settings' component={Settings} />
 
 					<Route path='/users' render={() => <UsersContainer />} />
+
+					<Route path='/login' component={Login} />
 				</div>
 				<div className="appContainer-aside">
 					<Navbar />
