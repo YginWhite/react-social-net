babf8dec91c31f92895505ecf0ed8a7156b18f1d
Yrii Sklyarov <yginwhite@gmail.com>
2020-02-14 19:27:49 +0200

    Create simple login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import styles from './Login.module.css';
 
 const Login = (props) => {
 	return (
-		<div>
+		<div className={styles.loginCont}>
 			<h1>Login</h1>
+			<LoginForm />
 		</div>
+	);
+};
+
+const LoginForm = (props) => {
+	return (
+		<form className={styles.loginForm}>
+			<div>
+				<input type="text" placeholder="Login"/>
+			</div>
+			<div>
+				<input type="text" placeholder="Password"/>
+			</div>
+			<div>
+				<input type="checkbox"/>Remember me
+			</div>
+			<div>
+				<button>Login</button>
+			</div>
+		</form>
 	);
 };
 
