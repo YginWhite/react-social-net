b48c21eb234357e1f759df380115c486e9f0ed49
Yrii Sklyarov <yginwhite@gmail.com>
2020-11-26 13:28:45 +0200

    Add functionality for clicking items on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import style from './Navbar.module.css';
 import {NavLink} from 'react-router-dom';
 
+let prevClickedItem = null;
+const handleClick = (e) => {
+	const elem = e.target.parentNode;
+	elem.classList.add('current_page_item');
+	if (prevClickedItem) prevClickedItem.classList.remove('current_page_item');
+	prevClickedItem = elem;
+};
 
 const Navbar = () => {
 	return (
-
-
-		<div id="menu">
+		<div id="menu" onClick={handleClick}>
 			<ul className={style.menu}>
-				<li className="current_page_item">
+				<li>
 					<NavLink to='/profile'>Profile</NavLink>
 				</li>
 				<li>
