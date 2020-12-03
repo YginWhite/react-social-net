3e41e2e3efa13a40992d231847ab32564faa04ff
Yrii Sklyarov <yginwhite@gmail.com>
2020-12-04 00:05:41 +0200

    Сlear excess imports

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import Login from './components/Login/Login';
 
-// import './App.css';
-// import HeaderContainer from './components/Header/HeaderContainer';
-// import Navbar from './components/Navbar/Navbar';
-// import ProfileContainer from './components/Profile/ProfileContainer';
-// import DialogsContainer from './components/Dialogs/DialogsContainer';
-// import News from './components/News/News';
-// import Music from './components/Music/Music';
-// import Settings from './components/Settings/Settings';
-// import UsersContainer from './components/Users/UsersContainer';
-// import Login from './components/Login/Login';
 
 const App = (props) => {
 	return (
