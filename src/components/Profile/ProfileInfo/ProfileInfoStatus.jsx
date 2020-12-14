0e279f1d59bb63e2c2bdabc49fea682dbe7c587b
Yrii Sklyarov <yginwhite@gmail.com>
2020-12-14 18:40:14 +0200

    Change PrifileInfoStatus component from class to function with hooks

--- src/components/Profile/ProfileInfo/ProfileInfoStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfoStatus.jsx
@@ -1,49 +1,80 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
-class ProfileInfoStatus extends React.Component {
-	state = {
-		editMode: false,
-		status: this.props.status
-	}
+const ProfileInfoStatus = (props) => {
+	let [editMode, setEditMode] = useState(false);
+	let [status, setStatus] = useState(props.status);
 
-	enableEditMode() {
-		this.setState({editMode: true});
-	}
+	const enableEditMode = () => setEditMode(true);
+	const disableEditMode = () => {
+		setEditMode(false);
+		props.changeStatus(status);
+	};
 
-	disableEditMode() {
-		this.setState({editMode: false});
-		this.props.changeStatus(this.state.status);
-	}
+	const onStatusChange = (e) => setStatus(e.currentTarget.value);
 
-	onStatusChange = (e) => {
-		this.setState({
-			status: e.currentTarget.value
-		});
-	}
+	useEffect(() => {
+		setStatus(props.status);
+	}, [props.status]);
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.status !== this.props.status) {
-			this.setState({
-				status: this.props.status
-			});
-		}
-	}
+	return (
+		<span>
+			{
+				editMode
+					? <span>
+						  <input autoFocus={true} 
+								 onBlur={disableEditMode} type="text" value={status}
+								 onChange={onStatusChange}/>
+					  </span>
+					: <span onDoubleClick={enableEditMode}>{status || 'no status'}</span>
+			}
+		</span>
+	);
+};
 
-	render() {
-		return (
-			<span>
-				{
-					this.state.editMode
-						? <span>
-							  <input autoFocus={true} 
-									 onBlur={this.disableEditMode.bind(this)} type="text" value={this.state.status}
-									 onChange={this.onStatusChange}/>
-						  </span>
-						: <span onDoubleClick={this.enableEditMode.bind(this)}>{this.props.status || 'no status'}</span>
-				}
-			</span>
-		);
-	}
-}
+// class ProfileInfoStatus extends React.Component {
+// 	state = {
+// 		editMode: false,
+// 		status: this.props.status
+// 	}
+
+// 	enableEditMode() {
+// 		this.setState({editMode: true});
+// 	}
+
+// 	disableEditMode() {
+// 		this.setState({editMode: false});
+// 		this.props.changeStatus(this.state.status);
+// 	}
+
+// 	onStatusChange = (e) => {
+// 		this.setState({
+// 			status: e.currentTarget.value
+// 		});
+// 	}
+
+// 	componentDidUpdate(prevProps, prevState) {
+// 		if (prevProps.status !== this.props.status) {
+// 			this.setState({
+// 				status: this.props.status
+// 			});
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<span>
+// 				{
+// 					this.state.editMode
+// 						? <span>
+// 							  <input autoFocus={true} 
+// 									 onBlur={this.disableEditMode.bind(this)} type="text" value={this.state.status}
+// 									 onChange={this.onStatusChange}/>
+// 						  </span>
+// 						: <span onDoubleClick={this.enableEditMode.bind(this)}>{this.props.status || 'no status'}</span>
+// 				}
+// 			</span>
+// 		);
+// 	}
+// }
 
 export default ProfileInfoStatus;
