9af68c43c5d9c4774f439adb60abd52116b85622
Yrii Sklyarov <yginwhite@gmail.com>
2021-04-20 23:02:19 +0300

    Extract custom styles to its own file

--- src/views/Profile/PostForm.js
+++ src/views/Profile/PostForm.js
@@ -9,40 +9,8 @@ import CardFooter from "components/Card/CardFooter.js";
 import CustomInput from "components/CustomInput/CustomInput.js";
 import InputLabel from "@material-ui/core/InputLabel";
 import Button from "components/CustomButtons/Button.js";
+import { styles } from './styles';
 
-const styles = {
-  cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
-    },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF"
-    }
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1"
-    }
-  },
-  addPostBtn: {
-  	color: 'gray',
-  	backgroundColor: '#f2f3f4'
-  }
-};
 
 const useStyles = makeStyles(styles);
 
