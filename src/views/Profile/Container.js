a8e4109e02020d666497b2d444a91fc7cb4f82de
Yrii Sklyarov <yginwhite@gmail.com>
2021-04-21 21:03:21 +0300

    Protect profile from unlogged userd

--- src/views/Profile/Container.js
+++ src/views/Profile/Container.js
@@ -40,5 +40,5 @@ const mapStateToProps = (state) => {
 export default compose(
 	connect(mapStateToProps, { getProfile, getStatus, changeStatus }),
 	withRouter,
-	// withAuthRedirect
+	withAuthRedirect
 )(Container)
