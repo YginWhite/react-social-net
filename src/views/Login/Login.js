28ecea241d62e4eb4d7d312b31e6098a8c1c9e23
Yrii Sklyarov <yginwhite@gmail.com>
2021-04-21 14:53:48 +0300

    Use react-final-form to handle Login form actions

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Form, Field } from 'react-final-form';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // core components
@@ -34,16 +35,19 @@ const customStyles = {
 
 const useStyles = makeStyles({...styles, ...customStyles});
 
-const CheckboxInp = () => {
+const CheckboxInp = (input) => {
 	const classes = useStyles();
 	return (
 		<Checkbox
+			inputProps={{ ...input }}
+			name="remember"
 		  checkedIcon={<Check className={classes.checkedIcon} />}
 		  icon={<Check className={classes.uncheckedIcon} />}
 		  classes={{
 		    checked: classes.checked,
 		    root: classes.root
 		  }}
+		  checked={input.checked}
 		/>
 	);
 };
@@ -55,45 +59,81 @@ const Login = () => {
 		<GridContainer justify="center">
 			<GridItem xs={12} sm={12} md={6}>
 				<Card>
-					
 					<CardHeader color="primary" icon stats className={classes.header}>
 					  <CardIcon color="primary">
               <AccountBoxIcon />
             </CardIcon>
 					</CardHeader>
-					
-					<CardBody>
-						<GridContainer>
-							<GridItem xs={12} sm={12} md={12}>
-								<CustomInput
-								  labelText="Username"
-								  id="username"
-								  formControlProps={{
-								    fullWidth: true
-								  }}
-								/>
-							</GridItem>
-							<GridItem xs={12} sm={12} md={12}>
-								<CustomInput
-								  labelText="UserEmail"
-								  id="userEmail"
-								  formControlProps={{
-								    fullWidth: true
-								  }}
-								/>
-							</GridItem>
-							<GridItem xs={12} sm={12} md={12}>
-								<FormControlLabel
-									className={classes.checkboxLabel}
-				          control={<CheckboxInp />}
-				          label={<span className={classes.labelFontSize}>Remember Me</span>}
-					      />
-							</GridItem>
-						</GridContainer>
-					</CardBody>
-					<CardFooter>
-						<Button color="primary">Login</Button>
-					</CardFooter>
+
+					<Form
+						onSubmit={values => console.log(values)}
+						render={(props) => (
+							<form onSubmit={props.handleSubmit}>
+								<CardBody>
+									<GridContainer>
+
+										<GridItem xs={12} sm={12} md={12}>
+											<Field
+												name="username"
+												render={({ input, meta }) => (
+													<CustomInput
+													  labelText={(meta.touched && meta.error || "Username")}
+													  id="username"
+													  name="username"
+													  formControlProps={{
+													    fullWidth: true
+													  }}
+													  inputProps={{ ...input }}
+													  success={meta.touched && !meta.error}
+													  error={meta.touched && !!meta.error}
+													/>
+												)}
+											/>
+										</GridItem>
+
+										<GridItem xs={12} sm={12} md={12}>
+											<Field
+												name="email"
+												render={({ input, meta }) => (
+													<CustomInput
+													  labelText={(meta.touched && meta.error || "Email")}
+													  id="email"
+													  name="email"
+													  formControlProps={{
+													    fullWidth: true
+													  }}
+													  inputProps={{ ...input }}
+													  success={meta.touched && !meta.error}
+													  error={meta.touched && !!meta.error}
+													/>
+												)}
+											/>
+										</GridItem>
+
+										<GridItem xs={12} sm={12} md={12}>
+											<Field
+												name="remember"
+												type="checkbox"
+												render={({ input, meta }) => (
+													<FormControlLabel
+														className={classes.checkboxLabel}
+									          control={<CheckboxInp {...input} />}
+									          label={<span className={classes.labelFontSize}>Remember Me</span>}
+										      />
+												)}
+											/>
+										</GridItem>
+
+									
+									</GridContainer>
+								</CardBody>
+								<CardFooter>
+									<Button type="submit" color="primary">Login</Button>
+								</CardFooter>
+							</form>
+						)}
+					/>
+
 				</Card>
 			</GridItem>
 		</GridContainer>
