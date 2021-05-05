135f2501fe3a7036284788c909f4d5514f969802
Yrii Sklyarov <yginwhite@gmail.com>
2021-05-05 19:21:13 +0300

    Updates

---
+++ precache-manifest.2d299c6cb62cb255787cff81022f856c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23e2e870ebf1c11bb0152a305edc62c",
+    "url": "/react-social-net/index.html"
+  },
+  {
+    "revision": "eeb12a5097b4f2287fa7",
+    "url": "/react-social-net/static/css/3.d0176e96.chunk.css"
+  },
+  {
+    "revision": "6d1132d796e22be8a820",
+    "url": "/react-social-net/static/css/main.98a14cce.chunk.css"
+  },
+  {
+    "revision": "e9602b2da078bec57cb0",
+    "url": "/react-social-net/static/js/0.e8d8d0e2.chunk.js"
+  },
+  {
+    "revision": "eeb12a5097b4f2287fa7",
+    "url": "/react-social-net/static/js/3.a6e0fd3b.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/react-social-net/static/js/3.a6e0fd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48932c1cef8f62448cbb",
+    "url": "/react-social-net/static/js/4.585af445.chunk.js"
+  },
+  {
+    "revision": "fe91add41ebad02f2198",
+    "url": "/react-social-net/static/js/5.86665680.chunk.js"
+  },
+  {
+    "revision": "801fe0924be9eeff2a79",
+    "url": "/react-social-net/static/js/6.b038ddfd.chunk.js"
+  },
+  {
+    "revision": "4a72385c3f54de04b828",
+    "url": "/react-social-net/static/js/7.773c1ed0.chunk.js"
+  },
+  {
+    "revision": "47a634e84a3e933096c9",
+    "url": "/react-social-net/static/js/8.f052404d.chunk.js"
+  },
+  {
+    "revision": "6d1132d796e22be8a820",
+    "url": "/react-social-net/static/js/main.2778113d.chunk.js"
+  },
+  {
+    "revision": "9c5e6fdc85b883ea13a98890c3577dba",
+    "url": "/react-social-net/static/js/main.2778113d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1ec9da7bd59c1bcbcd",
+    "url": "/react-social-net/static/js/runtime-main.c4bae4ac.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/react-social-net/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/react-social-net/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "764e0bc52a95bc66804825f1656de9df",
+    "url": "/react-social-net/static/media/user.764e0bc5.jpg"
+  }
+]);
