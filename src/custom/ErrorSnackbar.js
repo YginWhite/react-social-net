5b483730edf389027b77e4dc81c6f19271055dc1
Yrii Sklyarov <yginwhite@gmail.com>
2021-05-14 21:51:39 +0300

    Fix component when closing snackbar message

--- src/custom/ErrorSnackbar.js
+++ src/custom/ErrorSnackbar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Snackbar from "components/Snackbar/Snackbar.js";
 import AddAlert from "@material-ui/icons/AddAlert";
 
 export default function ErrorSnackbar({ message }) {
-	const [open, setOpen] = React.useState(true);
+	const [open, setOpen] = useState(true);
+	const [msg, setMsg] = useState(message);
+
+	if (msg !== message) {
+		setOpen(true);
+		setMsg(message);
+	}
 
 	return (
 		<Snackbar
